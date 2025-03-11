7a6e2a8ff25ad9651d283b0466e38549ec61583d
Boravy CHIN <bchin@consultants-solutec.fr>
2025-03-11 16:38:56 +0100

    feat(route): routing between home page and survey page added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import Header from './components/Header';
 import Home from './pages/Home'
+import Survey from './pages/Home/Survey';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/survey" element={<Survey />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
