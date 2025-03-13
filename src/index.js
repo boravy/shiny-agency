57f4a0367082a1d9253f277dab36efecd18b7315
Boravy CHIN <bchin@consultants-solutec.fr>
2025-03-13 12:03:58 +0100

    feat(styles): night mode style added

--- src/index.js
+++ src/index.js
@@ -1,37 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { createGlobalStyle } from 'styled-components'
 
 import Error from './components/Error';
+import Footer from './components/Footer';
 import Freelances from './pages/Freelances';
 import Header from './components/Header';
 import Home from './pages/Home'
 import Results from './pages/Results';
 import Survey from './pages/Survey';
-
-const GlobalStyle = createGlobalStyle`
-    * {
-      font-family: 'Trebuchet MS', Helvetica, sans-serif;
-    }
-    body {
-      margin: 0;
-    }
-`
+import { ThemeProvider } from './utils/context'
+import GlobalStyle from './utils/style/GlobalStyle'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-      <GlobalStyle />
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/survey/:questionNumber" element={<Survey />} />
-        <Route path="/results" element={<Results />} />
-        <Route path="/freelances" element={<Freelances />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
+      <ThemeProvider>
+        <GlobalStyle />
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/survey/:questionNumber" element={<Survey />} />
+          <Route path="/results" element={<Results />} />
+          <Route path="/freelances" element={<Freelances />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+        <Footer />
+      </ThemeProvider>
     </Router>
   </React.StrictMode>
 );
